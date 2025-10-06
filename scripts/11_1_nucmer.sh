efe5d08a09caf8f06e8ac88e70d5c6ae881a0a2d
albertw1706 <123548023+albertw1706@users.noreply.github.com>
2025-10-06 22:13:01 +0200

    Add files via upload

---
+++ scripts/11_1_nucmer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=nucmer
+#SBATCH --mail-user=albert.widjaja@students.unibe.ch
+#SBATCH --mail-type=end
+#SBATCH --output=/data/users/awidjaja/assembly_course/output_nucmer_%j.o
+#SBATCH --error=/data/users/awidjaja/assembly_course/error_nucmer_%j.e
+#SBATCH --partition=pibu_el8
+
+in_dir="/data/users/awidjaja/assembly_course"
+out_dir="/data/users/awidjaja/assembly_course/nucmer_res"
+
+apptainer exec -B ${in_dir}:/input -B ${out_dir}:/output -B "/data" /containers/apptainer/mummer4_gnuplot.sif nucmer -p /output/flye_vs_ref -t 16 --breaklen 1000 --mincluster 1000 /input/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa /input/flye_res/assembly.fasta
+apptainer exec -B ${in_dir}:/input -B ${out_dir}:/output -B "/data" /containers/apptainer/mummer4_gnuplot.sif nucmer -p /output/hifiasm_vs_ref -t 16 --breaklen 1000 --mincluster 1000 /input/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa /input/hifiasm_res/ERR11437321_hifiasm.fa
+apptainer exec -B ${in_dir}:/input -B ${out_dir}:/output -B "/data" /containers/apptainer/mummer4_gnuplot.sif nucmer -p /output/lja_vs_ref -t 16 --breaklen 1000 --mincluster 1000 /input/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa /input/lja_res/ERR11437321/lja_assembly.fasta
+
+apptainer exec -B ${in_dir}:/input -B ${out_dir}:/output -B "/data" /containers/apptainer/mummer4_gnuplot.sif nucmer -p /output/flye_vs_hifiasm -t 16 --breaklen 1000 --mincluster 1000 /input/flye_res/assembly.fasta /input/hifiasm_res/ERR11437321_hifiasm.fa
+apptainer exec -B ${in_dir}:/input -B ${out_dir}:/output -B "/data" /containers/apptainer/mummer4_gnuplot.sif nucmer -p /output/hifiasm_vs_lja -t 16 --breaklen 1000 --mincluster 1000 /input/hifiasm_res/ERR11437321_hifiasm.fa /input/lja_res/ERR11437321/lja_assembly.fasta
+apptainer exec -B ${in_dir}:/input -B ${out_dir}:/output -B "/data" /containers/apptainer/mummer4_gnuplot.sif nucmer -p /output/lja_vs_flye -t 16 --breaklen 1000 --mincluster 1000 /input/lja_res/ERR11437321/lja_assembly.fasta /input/flye_res/assembly.fasta
+
---
+++ scripts/11_2_mummer.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=mummer
+#SBATCH --mail-user=albert.widjaja@students.unibe.ch
+#SBATCH --mail-type=end
+#SBATCH --output=/data/users/awidjaja/assembly_course/output_mummer_%j.o
+#SBATCH --error=/data/users/awidjaja/assembly_course/error_mummer_%j.e
+#SBATCH --partition=pibu_el8
+
+ref_sample_dir="/data/users/awidjaja/assembly_course"
+in_dir="/data/users/awidjaja/assembly_course/nucmer_res"
+out_dir="/data/users/awidjaja/assembly_course/mummer_res"
+
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  bash -lc 'delta-filter -1 /input/flye_vs_ref.delta > /output/flye_vs_ref.filtered.delta'
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  bash -lc 'delta-filter -1 /input/hifiasm_vs_ref.delta > /output/hifiasm_vs_ref.filtered.delta'
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  bash -lc 'delta-filter -1 /input/lja_vs_ref.delta > /output/lja_vs_ref.filtered.delta'
+
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "${ref_sample_dir}:/sample_ref" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  mummerplot -R /sample_ref/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa \
+             -Q /sample_ref/flye_res/assembly.fasta \
+             --prefix=/output/flye_vs_ref --filter --fat --layout --large -t png /output/flye_vs_ref.filtered.delta
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "${ref_sample_dir}:/sample_ref" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  mummerplot -R /sample_ref/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa \
+             -Q /sample_ref/hifiasm_res/ERR11437321_hifiasm.fa \
+             --prefix=/output/hifiasm_vs_ref --filter --fat --layout --large -t png /output/hifiasm_vs_ref.filtered.delta
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "${ref_sample_dir}:/sample_ref" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  mummerplot -R /sample_ref/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa \
+             -Q /sample_ref/lja_res/ERR11437321/lja_assembly.fasta \
+             --prefix=/output/lja_vs_ref --filter --fat --layout --large -t png /output/lja_vs_ref.filtered.delta
+
+
+
+
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  bash -lc 'delta-filter -1 /input/flye_vs_hifiasm.delta > /output/flye_vs_hifiasm.filtered.delta'
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  bash -lc 'delta-filter -1 /input/hifiasm_vs_lja.delta > /output/hifiasm_vs_lja.filtered.delta'
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  bash -lc 'delta-filter -1 /input/lja_vs_flye.delta > /output/lja_vs_flye.filtered.delta'
+
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "${ref_sample_dir}:/sample_ref" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  mummerplot -R /sample_ref/flye_res/assembly.fasta \
+             -Q /sample_ref/hifiasm_res/ERR11437321_hifiasm.fa \
+             --prefix=/output/flye_vs_hifiasm --filter --fat --layout --large -t png /output/flye_vs_hifiasm.filtered.delta
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "${ref_sample_dir}:/sample_ref" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  mummerplot -R /sample_ref/hifiasm_res/ERR11437321_hifiasm.fa \
+             -Q /sample_ref/lja_res/ERR11437321/lja_assembly.fasta \
+             --prefix=/output/hifiasm_vs_lja --filter --fat --layout --large -t png /output/hifiasm_vs_lja.filtered.delta
+
+apptainer exec -B "${in_dir}:/input" -B "${out_dir}:/output" -B "${ref_sample_dir}:/sample_ref" -B "/data" /containers/apptainer/mummer4_gnuplot.sif \
+  mummerplot -R /sample_ref/lja_res/ERR11437321/lja_assembly.fasta \
+             -Q /sample_ref/flye_res/assembly.fasta \
+             --prefix=/output/lja_vs_flye --filter --fat --layout --large -t png /output/lja_vs_flye.filtered.delta
+
+
+
+
+
