877564cbce28b9fa732dce9f2e1feb9cfd0a3043
albertw1706 <123548023+albertw1706@users.noreply.github.com>
2025-10-10 00:34:24 +0200

    Update 08_2_busco_plot.sh

--- scripts/08_2_busco_plot.sh
+++ scripts/08_2_busco_plot.sh
@@ -9,27 +9,19 @@
 #SBATCH --error=/data/users/awidjaja/assembly_course/error_nucmer_%j.e
 #SBATCH --mail-type=error,end
 
-
-# set variables
 WORKDIR="/data/users/awidjaja/assembly_course"
 BUSCO_DIR="${WORKDIR}/busco_res"
 OUT_DIR="${WORKDIR}/busco_res/all"
 
-# Load BUSCO module
 module load BUSCO/5.4.2-foss-2021a
-# create directory if not available
+
 mkdir -p $OUT_DIR
 
-# Find and copy all BUSCO summary files (since we used --auto-lineage, the lineage names will vary)
-echo "Looking for BUSCO summary files..."
 find $BUSCO_DIR -name "short_summary.*.txt" -exec cp {} $OUT_DIR/ \;
 
-# List what files we found
-echo "Summary files found:"
 ls -la $OUT_DIR/*.txt
 
 # generate plots using the BUSCO module's generate_plot.py
-echo "Generating BUSCO plots..."
 cd $OUT_DIR
 generate_plot.py -wd $OUT_DIR
 
